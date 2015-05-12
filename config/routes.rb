7c03574bc83270e117c89383499b6a24c673c655
Tucker Raney <tuckerraney321@gmail.com>
2015-05-11 21:45:55 -0500

    Added Hobbies and Adventures pages; formatted Navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root "pages#home"
   get "about" => "pages#about" #creates about_path
-  
+  get "adventures" => "pages#adventures" #creates adventures_path
+  get "hobbies" => "pages#hobbies" #creates hobbies_path
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
